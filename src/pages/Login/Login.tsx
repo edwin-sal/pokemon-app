66ef65b44b7c84846319dde27771adcf2067aa6b
Edwin Bañago <ej.banago00601@student.tsu.edu.ph>
2024-11-30 21:40:11 +0800

    Add grid for profile pictures.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonInput, IonModal, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonGrid, IonHeader, IonInput, IonModal, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer';
 import styles from './Login.module.css';
 import loginIcon from '../../../resources/images/whos-that-pokemon-logo.png';
@@ -6,8 +6,20 @@ import profilePicture from '../../../resources/images/default-profile.jpg';
 import { useState } from 'react';
 
 const Login: React.FC = () => {
-  const [profilePicture, setProfilePicture] = useState('../../../resources/images/default-profile.jpg');
+  const profileUrl = '../../../resources/images/profiles/';
+
+  const [profilePicture, setProfilePicture] = useState(`${profileUrl}default-profile.jpg`);
   const [isOpen, setIsOpen] = useState(false);
+  
+  const profilePictures = [
+    'profile1.webp',
+    'profile2.webp',
+    'profile3.webp',
+    'profile4.webp',
+    'profile5.webp',
+    'profile6.webp',
+    'diwata.webp',
+  ];
 
   return (
     <IonPage className={styles['loign-page']}>
@@ -61,11 +73,24 @@ const Login: React.FC = () => {
             </IonToolbar>
           </IonHeader>
           <IonContent className="ion-padding">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni illum quidem recusandae ducimus quos
-              reprehenderit. Veniam, molestias quos, dolorum consequuntur nisi deserunt omnis id illo sit cum qui.
-              Eaque, dicta.
-            </p>
+            <div className={styles['profile-pictures-container']}>
+              {profilePictures.map((fileName, index) => (
+                <IonButton 
+                  className={styles['fit-content-button']}
+                  key={index}
+                  onClick={() => {
+                    setProfilePicture(profileUrl + fileName)
+                    setIsOpen(false);
+                  }}
+                  id={fileName}>
+                  <img 
+                    src={profileUrl + fileName} 
+                    alt="User profile picture" 
+                    className={styles['profile-picture']}
+                  />
+                </IonButton>
+              ))}
+            </div>
           </IonContent>
         </IonModal>
 
