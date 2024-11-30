704370d8c0b31d97907e544abce59fe7b7a3297c
Edwin Bañago <ej.banago00601@student.tsu.edu.ph>
2024-11-30 23:06:18 +0800

    Add function to add new users to localStorage.

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonInput, IonModal, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import { IonAlert, IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonContent, IonFab, IonFabButton, IonGrid, IonHeader, IonIcon, IonInput, IonModal, IonPage, IonRow, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../../components/ExploreContainer';
 import styles from './Login.module.css';
 import loginIcon from '../../../resources/images/whos-that-pokemon-logo.png';
@@ -9,8 +9,14 @@ import { add } from 'ionicons/icons';
 const Login: React.FC = () => {
   const profileUrl = '../../../resources/images/profiles/';
 
-  const [profilePicture, setProfilePicture] = useState(`${profileUrl}default-profile.jpg`);
+  const [profilePicture, setProfilePicture] = useState(`${profileUrl}profile1.webp`);
   const [isOpen, setIsOpen] = useState(false);
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+  const [userName, setUserName] = useState('');
+  const [errorInfo, setErrorInfo] = useState({
+    header: 'Undefined header',
+    message: 'Undefined message',
+  })
   
   const profilePictures = [
     'profile1.webp',
@@ -22,6 +28,74 @@ const Login: React.FC = () => {
     'diwata.webp',
   ];
 
+  const handleLogin = () => {
+    // Check if the user entered a username
+    if(userName.length === 0) {
+      setErrorInfo({
+        header: 'Invalid Username',
+        message: 'Please enter a valid username and try again',
+      });
+      setIsAlertOpen(true);
+      return;
+    }
+
+    // Set info to localStorage. Store if empty, update if not.
+    interface User {
+      username: string;
+      profilePicture: string;
+      score: number;
+    }
+
+
+    if (localStorage.getItem('users') === null) {
+      localStorage.setItem('users', JSON.stringify([{
+        username: userName,
+        profilePicture: profilePicture,
+        score: 0,
+      }]))
+    } 
+    
+    else {
+      const storedUsers = localStorage.getItem('users');
+      const data: User[] = storedUsers ? JSON.parse(storedUsers) : [];
+      
+      // Check if userName already exists
+      let userNameAlreadyExist = false;
+      for(let i = 0; i < data.length; i++) {
+        if(data[i].username === userName) {
+          // console.log(`${data[i].username} === ${userName}`)
+          userNameAlreadyExist = true;
+          break;
+        } 
+      }
+
+      // Show error message if username already exists
+      if(userNameAlreadyExist) {
+        setErrorInfo({
+          header: 'Invalid Username',
+          message: `Username "${userName} already exists! Please enter a new one."`,
+        });
+        setIsAlertOpen(true);
+        return;
+      }
+
+      // Insert user name to localStorage
+      const updatedData = [...data, {
+        username: userName,
+        profilePicture: profilePicture,
+        score: 0,
+      }]
+
+      localStorage.setItem('users', JSON.stringify(updatedData));
+    }
+  };
+
+  const handleUserNameChange = (event: CustomEvent) => {
+    setUserName(event.detail.value);
+  };
+
+  // console.log(userName);
+  // console.log(profilePicture)
   return (
     <IonPage className={styles['loign-page']}>
      <IonContent className={`${styles['login-background']} ion-padding`}>
@@ -50,6 +124,8 @@ const Login: React.FC = () => {
             <IonCardContent>
               <IonInput 
                 className='ion-text-center'
+                onIonInput={handleUserNameChange}
+                value={userName}
                 placeholder="Tap here to enter Username" 
                 aria-label="Text"
               ></IonInput>
@@ -57,6 +133,7 @@ const Login: React.FC = () => {
               <IonButton 
                 className="ion-margin-top" 
                 expand="full" 
+                onClick={handleLogin}
               >
                 Play Game
               </IonButton>
@@ -100,6 +177,14 @@ const Login: React.FC = () => {
             </IonFabButton>
           </IonFab>
         </IonModal>
+
+        <IonAlert
+          isOpen={isAlertOpen}
+          header={errorInfo.header}
+          message={errorInfo.message}
+          buttons={['Okay']}
+          onDidDismiss={() => setIsAlertOpen(false)}>
+        </IonAlert>
       </IonContent>
     </IonPage>
   );
